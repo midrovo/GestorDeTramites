e315e8f57fa51359d8f5b91b98f9317eb33603af
midrovo <midrovo96@outlook.com>
2024-04-05 23:08:36 -0500

    Iniciando Proyecto

---
+++ src/main/java/com/sptmf/GestorTramite/GestorTramiteApplication.java
@@ -0,0 +1,13 @@
+package com.sptmf.GestorTramite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestorTramiteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestorTramiteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sptmf/GestorTramite/GestorTramiteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sptmf.GestorTramite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GestorTramiteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
