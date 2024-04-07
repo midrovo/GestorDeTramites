fa3337a8dddd704cdf9dcb4e4ca7ed91c042185e
RPGrodri2001 <ginorodri2001@outlook.com>
2024-04-07 18:29:00 -0500

    se trabajo en las clases Categoria, Departamento, rol

--- src/main/java/com/sptmf/GestorTramite/model/Departamento.java
+++ src/main/java/com/sptmf/GestorTramite/model/Departamento.java
@@ -1,4 +1,23 @@
 package com.sptmf.GestorTramite.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "departamentos")
 public class Departamento {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "Description", length = 100, nullable = false)
+    private String detail;
+
+
+
 }
--- src/main/java/com/sptmf/GestorTramite/model/Rol.java
+++ src/main/java/com/sptmf/GestorTramite/model/Rol.java
@@ -1,4 +1,25 @@
 package com.sptmf.GestorTramite.model;
 
+
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "roles")
 public class Rol {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private long id;
+
+    @Column(name = "rol", length = 20, nullable = false)
+    private String rol;
+
+    @Column(name = "Description", length = 100, nullable = false)
+    private String detail;
+
+
 }
