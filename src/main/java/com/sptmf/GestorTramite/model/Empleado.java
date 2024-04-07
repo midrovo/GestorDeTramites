8de6dc5948f2a9585cedfbd74905ff238e0eda48
midrovo <midrovo96@outlook.com>
2024-04-07 18:28:55 -0500

    Se trabajo en las clases cliente, empleado, proceso, tramite y user.

--- src/main/java/com/sptmf/GestorTramite/model/Empleado.java
+++ src/main/java/com/sptmf/GestorTramite/model/Empleado.java
@@ -1,4 +1,21 @@
 package com.sptmf.GestorTramite.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "empleados")
 public class Empleado {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombres", length = 50, nullable = false)
+    private String name;
+
+    @Column(name = "apellidos", length = 50, nullable = false)
+    private String lastname;
 }
--- src/main/java/com/sptmf/GestorTramite/model/User.java
+++ src/main/java/com/sptmf/GestorTramite/model/User.java
@@ -1,5 +1,21 @@
 package com.sptmf.GestorTramite.model;
 
+import jakarta.persistence.*;
+import lombok.Data;
+import lombok.NoArgsConstructor;
 
+@Entity
+@Data
+@NoArgsConstructor
+@Table(name = "usuarios")
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "nombre_usuario", length = 20, nullable = false, unique = true)
+    private String username;
+
+    @Column(name = "clave", length = 15, nullable = false)
+    private String password;
 }
