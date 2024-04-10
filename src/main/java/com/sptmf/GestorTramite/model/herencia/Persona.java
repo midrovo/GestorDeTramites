ab288aa8d8243b46536e2a7a6e233f0d9e944ffe
midrovo <midrovo96@outlook.com>
2024-04-09 20:21:47 -0500

    Actualizando repositorio

--- src/main/java/com/sptmf/GestorTramite/model/herencia/Persona.java
+++ src/main/java/com/sptmf/GestorTramite/model/herencia/Persona.java
@@ -1,7 +1,5 @@
 package com.sptmf.GestorTramite.model.herencia;
 
-import com.fasterxml.jackson.annotation.JsonProperty;
-import com.sptmf.GestorTramite.model.User;
 import jakarta.persistence.*;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +8,10 @@ import lombok.NoArgsConstructor;
 @NoArgsConstructor
 @MappedSuperclass
 public class Persona {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
     @Column(name = "nombres", length = 50, nullable = false)
     private String name;
 
